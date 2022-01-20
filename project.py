71529c6dc83a03119f87c11625589508b5f89442
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-01-20 22:38:14 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+#Let's program together!! we are going to build a learning platform for you using python!!
+
+#let's build robot 
+
+print("Hello welcome to Nmuhra learning!!")
+
+name= input("Name?\n")
+
+print("Hello "+ name +", thank you for joining us today.")
+
+menu = "Python, Javascript, R, CSS"
+
+print(name + ", what do you wanna learn today? Here are the options.\n "+ menu)
+
+order = input()
+
+print("Great " + name + ", We will start shortly with your " + order +" course, get your coffee ready")
