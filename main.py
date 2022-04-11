3bcac503034ba77fe437a802829eee80b60f4629
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-04-11 20:26:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-print("Hello welcome to Nmuhra learning!!")
-name= input("Name?\n")
-print("Hello "+ name +", thank you for joining us today.")
-for PL in ["python, javascript, R, react"]:
-  choice = input(f"which programming language do you want to learn: {PL}\n").lower
-  print(f"Great {name}, We will start shortly with your {choice} course, get your coffee ready")
-language = "English, French, Spanish"
-print(name + ", what language do you want your course to be in? Here are the ones currently available.\n"+ language)
-language = input().lower()
-if language == "english":
-    print(f"thanks {name}, we are finalizing your {choice} course, making sure we are using {language}")
-if language == "french":
-    print(f"merci {name}, nous finalisons votre cours {choice}, en nous assurant que nous utilisons {language}")
-if language == "spanish":
-    print(f"gracias {name}, estamos finalizando su {choice} curso, asegurándonos de que estamos usando {language}")
+import re
+import long_responses as long
+
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+  
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
+    response(long.R_EATING, ['inside', 'heel', 'hook'], required_words=['heel', 'hook'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+while True:
+    print('Bot: ' + get_response(input('You: ')))
